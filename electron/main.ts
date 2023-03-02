369689acd915af64c63e38572dd9aed2ae61f858
hyunsol1025 <54145442+hyunsolcomment@users.noreply.github.com>
2023-03-02 20:46:00 +0900

    .

---
+++ electron/main.ts
@@ -0,0 +1,38 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 700,
+    webPreferences: {
+      // contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  if (app.isPackaged) {
+    // 'build/index.html'
+    win.loadURL(`file://${__dirname}/../index.html`);
+  } else {
+    win.loadURL('http://localhost:3000/index.html');
+
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+});
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      TRPG 클라이언트
+    </div>
+  );
+}
+
+export default App;
