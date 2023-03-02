e62b707febf711773bf5570178c0d7b80cb63497
hyunsol1025 <54145442+hyunsolcomment@users.noreply.github.com>
2023-03-02 20:46:35 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      TRPG 클라이언트
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
